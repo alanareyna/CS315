ea44ee4b7d202e276ff336fa28cc4efd9cf9379f
Alana Reyna <60456837+alanareyna@users.noreply.github.com>
2022-05-23 15:48:54 -0700

    Create Tokenizer.hpp

---
+++ lab1/phase2/Tokenizer.hpp
@@ -0,0 +1,29 @@
+//                                                                                                                                                                                                          
+// Created by Alana Reyna on 8/21/18.                                                                                                                                                                   
+//                                                                                                                                                                                                          
+
+#ifndef LAB01_2_TOKENIZER_HPP
+#define LAB01_2_TOKENIZER_HPP
+
+
+#include <string>
+#include <fstream>
+#include "Token.hpp"
+
+class Tokenizer {
+
+public:
+    //std::vector<int> stack;                                                                                                                                                                               
+    Tokenizer(std::string);
+    Token getToken();
+
+private:
+    int lineNumber, charPosition;
+    std::string inputFileName;
+    std::ifstream inputStream;
+
+    bool charOfInterest(char c);
+};
+
+
+#endif //LAB01_2_TOKENIZER_HPP       
