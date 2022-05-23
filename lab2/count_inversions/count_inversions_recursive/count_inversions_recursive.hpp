efa52f86748f610a12c217413a82224201d6a44f
Alana Reyna <60456837+alanareyna@users.noreply.github.com>
2022-05-23 15:57:20 -0700

    Create count_inversions_recursive.hpp

---
+++ lab2/count_inversions/count_inversions_recursive/count_inversions_recursive.hpp
@@ -0,0 +1,18 @@
+//                                                                                                                                                                                                          
+// Created by Alana Reyna on 9/17/19.                                                                                                                                                                       
+//                                                                                                                                                                                                          
+
+#ifndef COUNT_INVERSIONS_COUNT_INVERSIONS_RECURSIVE_HPP
+#define COUNT_INVERSIONS_COUNT_INVERSIONS_RECURSIVE_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+int count_inversions_recursive(const std::vector<int> &numbers);
+
+int count(const std::vector<int> &numbers);
+
+#endif //COUNT_INVERSIONS_COUNT_INVERSIONS_RECURSIVE_HPP                                                                                                                                                    
+
