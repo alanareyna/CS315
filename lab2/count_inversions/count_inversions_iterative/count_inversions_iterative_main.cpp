8c8d950ff7e150c31c5f905f72b7e6ea8f636589
Alana Reyna <60456837+alanareyna@users.noreply.github.com>
2022-05-23 15:51:51 -0700

    Create count_inversions_iterative_main.cpp

---
+++ lab2/count_inversions/count_inversions_iterative/count_inversions_iterative_main.cpp
@@ -0,0 +1,20 @@
+//                                                                                                                                                                                                          
+// Created by Alana Reyna on 9/17/19.                                                                                                                                                                       
+//                                                                                                                                                                                                          
+#include<iostream>
+#include<string>
+#include<vector>
+#include "read_input.hpp"
+#include "count_inversions_iterative.hpp"
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cout << "usage:" << argv[0] << " nameOfInputFile\n";
+        exit(2);
+    }
+    std::vector<int> numbers, result;
+    read_numbers(std::string(argv[1]), numbers);
+
+        std::cout << count_inversions_iterative(numbers) << std::endl;
+    return 0;
+}
