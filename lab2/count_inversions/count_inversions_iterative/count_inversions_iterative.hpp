e531fe24eff0688cc89d37c0f4e7eb153f652844
Alana Reyna <60456837+alanareyna@users.noreply.github.com>
2022-05-23 15:52:56 -0700

    Create count_inversions_iterative.hpp

---
+++ lab2/count_inversions/count_inversions_iterative/count_inversions_iterative.hpp
@@ -0,0 +1,15 @@
+//                                                                                                                                                                                                          
+// Created by Alana Reyna on 9/17/19.                                                                                                                                                                       
+//                                                                                                                                                                                                          
+
+#ifndef COUNT_INVERSIONS_COUNT_INVERSIONS_ITERATIVE_HPP
+#define COUNT_INVERSIONS_COUNT_INVERSIONS_ITERATIVE_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+int count_inversions_iterative(const std::vector<int> &numbers);
+
+#endif //COUNT_INVERSIONS_COUNT_INVERSIONS_ITERATIVE_HPP  
