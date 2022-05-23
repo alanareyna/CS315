76d1cd170e4db2415acb7fb5c407b640aaa9eec2
Alana Reyna <60456837+alanareyna@users.noreply.github.com>
2022-05-23 15:53:50 -0700

    Create read_input.hpp

---
+++ lab2/count_inversions/count_inversions_iterative/read_input.hpp
@@ -0,0 +1,15 @@
+//                                                                                                                                                                                                          
+// Created by Alana Reyna on 9/17/19.                                                                                                                                                                       
+//                                                                                                                                                                                                          
+
+#ifndef COUNT_INVERSIONS_READ_INPUT_HPP
+#define COUNT_INVERSIONS_READ_INPUT_HPP
+
+#include <string>
+
+
+void read_numbers(std::string filename, std::vector<int> &v);
+
+
+#endif //COUNT_INVERSIONS_READ_INPUT_HPP                                                                                                                                                                    
+
